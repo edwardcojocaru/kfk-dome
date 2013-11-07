31f365cadba5ef2386a0f7f042b520eaccbd59b5
Eduard Cojocaru <eduard.cojocaru@orange.com>
2013-11-07 16:10:50 +0200

    add logs

--- kafka-consumer/src/main/java/com/francetelecom/dome/consumer/configuration/ConfigurableFactory.java
+++ kafka-consumer/src/main/java/com/francetelecom/dome/consumer/configuration/ConfigurableFactory.java
@@ -1,6 +1,8 @@
 package com.francetelecom.dome.consumer.configuration;
 
 import com.francetelecom.dome.consumer.utils.Constants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -12,14 +14,16 @@ import java.util.ResourceBundle;
  */
 public final class ConfigurableFactory {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConfigurableFactory.class);
     private ConfigurableFactory() {
     }
 
     public static Configurable getConfigurable(String configurationPath) throws IOException {
         if (configurationPath != null) {
+            LOGGER.info("Loading stream configuration from {}", configurationPath);
             return new StreamConfiguration(new FileInputStream(configurationPath));
-//            return new StreamConfiguration(Files.newInputStream(Paths.get(configurationPath), new OpenOption[]{StandardOpenOption.READ}));
         } else {
+            LOGGER.info("Loading bundle configuration from jar.");
             return new BundleConfiguration(ResourceBundle.getBundle(Constants.CONFIGURATION_FILE_BASE_NAME));
         }
     }
